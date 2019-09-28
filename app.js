8e573933ac233cbb6a1ffaf16a4d88d68eb5c61a
Student <student@itpark>
2019-09-28 10:42:48 +0300

    фото видео

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log('worked');
+// выкидываем всё лишнее, что не нужно для решения
+const author = {
+    id: 'buzova86',
+    avatarUrl: https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com
+    verified: true,
+}
+const photo = {
+id: 'B23d82NooNF',
+author: author,
+location: 'Набережная Амура. Граница с Китаем',
+imageUrl: 'https://placetitten.com/800/800',
+videoUrl: null,
+likes: 159594,
+
+};
+//undfined и null можно записать где угодно
+//пост с видео
+const video = {
+    id: 'B23d82NooNX',
+    author: author,
+    location: 'Тщргово-развлекательный комплекс Острова',
+    image: 'https://placetitten.com/800/800',
+    videoUrl = 'https://youtube.com/...',
+    likes: 159594, 
+};
